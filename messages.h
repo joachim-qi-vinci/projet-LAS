caa1c674cfd6562413c039326d8df490601c510a
Qi Joachim <129367758+joachim-qi-vinci@users.noreply.github.com>
2024-04-18 12:21:39 +0200

    Update messages.h

--- messages.h
+++ messages.h
@@ -7,9 +7,15 @@
 
 typedef enum
 {
-  INSCRIPTION_REQUEST = 10,
-  INSCRIPTION_OK = 11,
-  INSCRIPTION_KO = 12
+  INSCRIPTION_REQUEST = 1,
+  INSCRIPTION_OK = 200,
+  INSCRIPTION_KO = 400,
+  PARTIE_LANCEE = 201,
+  NOUVELLE_TUILE = 202,
+  DEMANDER_SCORE = 210,
+  NOTER_SCORE = 211,
+  FIN_DE_PARTIE = 212,
+  PARTIE_ANNULEE = 300,
 } Code;
 
 /* struct message used between server and client */
