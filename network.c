f542d9a7c769b10fb997f43e977012d32709b929
Jo <joachiim.qi@student.vinci.be>
2024-04-25 13:42:54 +0200

    push

--- network.c
+++ network.c
@@ -1,4 +1,37 @@
-void endServerHandler(int sig)
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "network.h"
+#include "server.h"
+
+
+void disconnect_players(Player *tabPlayers, int nbPlayers)
 {
-    end_inscriptions = 1;
+    for (int i = 0; i < nbPlayers; i++)
+        sclose(tabPlayers[i].sockfd);
+    return;
+}
+
+int initSocketServer(int port)
+{
+    int sockfd = ssocket();
+
+    /* no socket error */
+
+    // setsockopt -> to avoid Address Already in Use
+    // to do before bind !
+    int option = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(int));
+
+    sbind(port, sockfd);
+
+    /* no bind error */
+    slisten(sockfd, BACKLOG);
+
+    /* no listen error */
+    return sockfd;
 }
--- network.h
+++ network.h
@@ -1,7 +1,15 @@
 #ifndef _NETWORK_H_
 #define _NETWORK_H_
 
+#include "messages.h"
 
-void endServerHandler(int sig);
+void disconnect_players(Player *tabPlayers, int nbPlayers);
+/**
+ * PRE:  serverPort: a valid port number
+ * POST: on success, binds a socket to 0.0.0.0:serverPort and listens to it ;
+ *       on failure, displays error cause and quits the program
+ * RES:  return socket file descriptor
+ */
+int initSocketServer(int port);
 
 #endif
