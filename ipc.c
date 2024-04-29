bd8e22a8b263b84339d37b05b7a82ea164868c81
robin-salle-vinci <robin.salle@student.vinci.be>
2024-04-29 12:47:13 +0200

    test

--- ipc.c
+++ ipc.c
@@ -1,29 +1,31 @@
 #include <stdlib.h>
+#include <string.h>
 #include "ipc.h"
 #include "utils_v1.h"
 
 int shm_id;
 int sem_id;
+Player* tabPlayer;
 
 void createScoresTab(int nbr_player) {
-    Player* tabPlayer = (Player*) smalloc(nbr_player * sizeof(Player));
+    tabPlayer = (Player*) smalloc(nbr_player * sizeof(Player));
     shm_id = sshmget(SHM_KEY, sizeof(tabPlayer), IPC_CREAT | PERM);
-    tabPlayer = sshmat(shm_id);
+    Player* tabPlayerIPC = sshmat(shm_id);
+    memcpy(tabPlayerIPC, tabPlayer, nbr_player * sizeof(Player));
+    sshmdt(tabPlayerIPC);
     sem_id = sem_create(SEM_KEY, 1, PERM, 0);
-    sshmdt(tabPlayer);
 }
 
 
 void placeScore(Player player, int logical_size) {
     sem_down0(sem_id);
-    Player* tabPlayer = sshmat(shm_id);
-    tabPlayer[logical_size] = player;
+    Player* tabPlayerIPC = sshmat(shm_id);
+    tabPlayerIPC[logical_size] = player;
     sem_up0(sem_id);
-    sshmdt(tabPlayer);
+    sshmdt(tabPlayerIPC);
 }
 
 void closeIPC() {
-    Player* tabPlayer = sshmat(shm_id);
     free(tabPlayer);
     sshmdelete(shm_id);
     sem_delete(sem_id);
